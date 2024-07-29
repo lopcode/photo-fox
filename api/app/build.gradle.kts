d9b981c92c43fdd836839fbd3cc3709f626ea36a
carrot <149851+lopcode@users.noreply.github.com>
2024-07-30 00:26:06 +0100

    Add a basic api project (#1)

---
+++ api/app/build.gradle.kts
@@ -0,0 +1,98 @@
+@file:Suppress("UnstableApiUsage")
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+    id("io.ktor.plugin") version "3.0.0-beta-2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useKotlinTest("2.0.0")
+            testType = TestSuiteType.UNIT_TEST
+
+            dependencies {
+                implementation("io.ktor:ktor-server-test-host")
+            }
+        }
+
+        register<JvmTestSuite>("integrationTest") {
+            useKotlinTest("2.0.0")
+            testType = TestSuiteType.INTEGRATION_TEST
+
+            dependencies {
+                implementation(project())
+            }
+
+            targets {
+                all {
+                    testTask.configure {
+                        shouldRunAfter(test)
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<Test> {
+    testLogging {
+        events = TestLogEvent.values().toSet() - TestLogEvent.STARTED
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+    outputs.upToDateWhen { false }
+}
+
+tasks.named("check") {
+    dependsOn(testing.suites.named("test"))
+    dependsOn(testing.suites.named("integrationTest"))
+}
+
+tasks.withType<ShadowJar> {
+    mergeServiceFiles {
+        setPath("META-INF/services/org.flywaydb.core.extensibility.Plugin")
+    }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-call-id")
+    implementation("io.ktor:ktor-server-forwarded-header")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-mustache")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("org.flywaydb:flyway-core:10.16.0")
+    implementation("org.flywaydb:flyway-database-postgresql:10.15.2")
+    implementation(platform("org.jdbi:jdbi3-bom:3.45.2"))
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("org.jdbi:jdbi3-core")
+    implementation("org.jdbi:jdbi3-sqlobject")
+    implementation("org.jdbi:jdbi3-kotlin")
+    implementation("org.jdbi:jdbi3-kotlin-sqlobject")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+}
+
+application {
+    mainClass = "com.photofox.MainKt"
+}
---
+++ api/app/src/integrationTest/kotlin/com/photofox/SenseCheck.kt
@@ -0,0 +1,13 @@
+package com.photofox
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SenseCheck {
+    @Test
+    fun `sense check`() {
+        assertTrue(true)
+        assertFalse(false)
+    }
+}
